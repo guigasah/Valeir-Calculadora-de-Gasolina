82f5296ca803837a0ad2c38fed4db8d27131e4ee
Guilherme Sá <103279555+guigasah@users.noreply.github.com>
2022-04-08 19:46:26 -0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -17,11 +17,6 @@ function calcular() {
     resultado.innerHTML = `<p>${Math.ceil(calcgas)} Reais você vai gastar para ir até lá!</p>`
 }
 
-//INNERHTML- O primeiro inner vai mostrar na tela o resultado do calculo
-//           O segundo inner vai mostrar o Resultado da função criada para denominar a situação em peso do usuário
-//resultado.innerHTML = `<p class="imc">Seu IMC é:  ${calcImc .toFixed(2) }</p>`;
-//  resultado.innerHTML += `<p class="imc2">${verificaNivel(calcImc)}</p>`
-
 
 //Função pra limpar os campos preenchidos e o Resultado
 function limpar() {
